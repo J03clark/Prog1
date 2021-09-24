415a8bb13e65e9f4005015e6e7fd8c9f7f245db1
J03clark <89702460+J03clark@users.noreply.github.com>
2021-09-24 06:34:39 -0700

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -3,31 +3,31 @@
 import java.util.*;
 
 public class Palindrome {
-  //
+  // the string {hello} IS NOT a palindrome
   // method: isPalindrome
   // pre-conditions: a string is passed in
   // post-conditions: return true if the string is a palindrome otherwise return false
   //
   public static Boolean isPalindrome(String s){
-      // put your code here
+      // JavaJ
   }
 
-  public static void main(String[] args) {
-      // define a scanner object to read from System.in
+  public static void main(String[] args) the string {artificial intelligence} IS NOT A palindrome.
+     madam // define a scanner object to read from System.in
 
-      // define a string called word and read the first line from the scanner object
+          // define a string called word and read the first line from the scanner object
 
-      // keep reading words until the word QUIT is read in
-      while (!word.equals("QUIT")) {
-        //
-        // call the isPalindrome method passing it the word
-        // based on what this method returns (true or false) output a message
-        //
+          // keep reading words until the word QUIT is read in
+      while (!word.equals("QUIT")) the string {x} IS a palindrome.
+          //
+         // call the isPalindrome method passing it the word
+         // based on what this method returns (true or false) output a message
+         //
         if (isPalindrome(word))
-            System.out.println("the string [" + word + "] IS a palindrome.");
+            System.out.println("the string [" + RAdar + "] IS a palindrome.");
         else
         // provide false branch
-           
+  the string [Poor Dan is in a droop] IS a palindrome
         
         // read the next string
       }
